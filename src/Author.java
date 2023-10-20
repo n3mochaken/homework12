94d9bb6a8830d8e7a0f83bfab95ce096925de98d
n3mochaken <ns.mixa@gmail.com>
2023-10-20 17:31:37 +0300

    Homework 13 done!

--- src/Author.java
+++ src/Author.java
@@ -10,9 +10,24 @@ public class Author {
     public String getName() {
         return firstName;
     }
-
     public String getSecondName() {
         return lastName;
+    }
+    @Override
+    public int hashCode() {
+        return java.util.Objects.hash(firstName,lastName);
+    }
+    @Override
+    public String toString(){
+        return firstName+" "+ lastName;
+    }
+    @Override
+    public boolean equals(Object other) {
+        if (this.getClass() != other.getClass()) {
+            return false;
+        }
+        Author c2 = (Author) other;
+        return lastName.equals(c2.lastName);
     }
 
 
--- src/Book.java
+++ src/Book.java
@@ -24,4 +24,23 @@ public class Book {
     public void setYear(int year) {
         this.year = year;
     }
+
+    @Override
+    public String toString() {
+        return "Название книги - " + title + ". Автор - " + author + ". Год печати- " + year;
+    }
+
+    @Override
+    public int hashCode() {
+        return java.util.Objects.hash(title, author);
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if (this.getClass() != other.getClass()) {
+            return false;
+        }
+        Book c2 = (Book) other;
+        return title.equals(c2.title);
+    }
 }
